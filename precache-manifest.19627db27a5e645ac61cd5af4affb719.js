41d83b9d981939ebc9b3be74cc8e3be8f778889f
Ashish Subedi <ashishsubedi10@outlook.com>
2019-07-27 22:09:59 +0545

    Updates

---
+++ precache-manifest.19627db27a5e645ac61cd5af4affb719.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "852c43d9c731b8585623e77e1fe61a4b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "24cda1a0a015f4d7f0ed",
+    "url": "/monsters-rolodex/static/css/main.c8e71a8f.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "24cda1a0a015f4d7f0ed",
+    "url": "/monsters-rolodex/static/js/main.6420fd1c.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
